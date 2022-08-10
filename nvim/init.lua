4e5e85ec11872795098af6af89fed19fd8369816
Noah Mannberg <noah.mannberg@gmail.com>
2022-08-10 10:45:16 +0200

    init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,4 @@
--------------------- HELPERS --------------------
+--------------------- HELPERS --------------------
 local cmd = vim.cmd  -- to execute Vim commands e.g. cmd('pwd')
 local fn = vim.fn    -- to call Vim functions e.g. fn.bufnr()
 local g = vim.g      -- a table to access global variables
@@ -17,6 +17,7 @@ opt.guicursor = ""
 
 opt.number = true                   -- show line numbers
 
+opt.visualbell = true
 
 opt.tabstop = 4                     -- number of spaces tabs count for
 opt.softtabstop = 4
@@ -42,6 +43,17 @@ opt.mouse = "a"
 
 g.mapleader = " "
 
+-- enable undos between sessions
+cmd([[set undofile]])
+
+-- enable that we go back to last position in file
+cmd [[
+if has("autocmd")
+  au BufReadPost * if line("'\"") > 0 && line("'\"") <= line("$")
+    \| exe "normal! g'\"" | endif
+endif
+]]
+
 -- netrw settings
 g.netrw_banner = 0                  -- disable banner
 g.newrw_winsize = 25                -- explore is 25% of screen
@@ -62,6 +74,8 @@ local packer = require('packer').startup(function(use)
 
     use {'neoclide/coc.nvim', branch = 'release'}
 
+    use {"terrortylor/nvim-comment"}
+
     use {'nvim-telescope/telescope.nvim', tag = '0.1.0',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
@@ -78,7 +92,7 @@ cmd 'colorscheme tender'
 -------------------- TREE-SITTER ----------------
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "c", "lua", "rust" },
+  ensure_installed = { "c", "cpp", "lua", "rust" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
@@ -92,20 +106,25 @@ require'nvim-treesitter.configs'.setup {
 
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
--- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
 }
 
 -------------------- COC ------------------------
-
 opt.encoding = "utf-8"
 opt.hidden = true
 opt.cmdheight = 2
 opt.updatetime = 300    
 opt.signcolumn = "number"
 
+-------------------- NVIM COMMENT ---------------
+require('nvim_comment').setup {
+    line_mapping = "<leader>cc",
+    operator_mapping = "<leader>c"
+    --comment_chunk_text_object = "",
+}
 
 -------------------- TELESCOPE  -----------------
 require('telescope').setup{
@@ -124,17 +143,16 @@ require('telescope').setup{
 map('n', '<leader>ff', "<cmd>lua require('telescope.builtin').find_files()<cr>") -- telescope find files
 map('n', '<leader>fb', "<cmd>lua require('telescope.builtin').buffers()<cr>") -- telescope find files
 
-
 -------------------- LUALINE --------------------
 require('lualine').setup {
     options = {
         icons_enabled = false,
-        theme = 'wombat'
+        theme = 'seoul256'
     }
 }
 
 -------------------- MAPPINGS -------------------
-map('n', '<leader>o', 'o<Esc>')                     -- insert newline from normal mode
-map('n', '<leader>e', '<cmd>Vexplore<CR>')              -- show file explorer
-
+map('n', '<leader>o', 'o<Esc>')                 -- insert newline from normal mode
+map('n', '<leader>O', 'O<Esc>')                 -- insert newline from normal mode
+map('n', '<leader>e', '<cmd>Vexplore<CR>')      -- show file explorer
 
